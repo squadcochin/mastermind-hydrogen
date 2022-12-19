066adfa98162de767494ec6d2784ff6d0bc0a8b7
sarika <sarikasasikumar21@gmail.com>
2022-12-19 11:35:14 +0530

    created new component

--- src/components/header/announcement/announcement.client.jsx
+++ src/components/header/announcement/announcement.client.jsx
@@ -1,7 +1,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import "style.css";
+
 
 export default function Announcement() {
     var settings = {
