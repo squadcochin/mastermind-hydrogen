cf5480bb750ef372c5d608b8b7688fea418b832b
sarika <sarikasasikumar21@gmail.com>
2022-12-19 17:23:05 +0530

    added menu

--- src/components/header/announcement/announcement.client.jsx
+++ src/components/header/announcement/announcement.client.jsx
@@ -1,32 +1,37 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
+import './style.css';
+import Menu from "../topnavigation/Menu.client";
 
 export default function Announcement() {
     var settings = {
         dots: false,
         infinite: true,
         speed: 500,
+        autoplay:true,
+        arrows:false,
         slidesToShow: 1,
         slidesToScroll: 1
       };
 
     return (
-      <div class="container-2xl">
-        <div class="announcement-bar-bg">
+      <div className="container-2xl">
+        <div className="announcement-bar-bg">
         <Slider {...settings}>
-            <div class="mx-auto">
-                <h3 class="text-center">1</h3>
+            <div className="mx-auto">
+                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">FREE SHIPPING* on  mjyuiy  orders over $35 (shipped within Canada, some exceptions apply)</h3>
             </div>
-            <div  class="mx-auto">
-                <h3 class="text-center">2</h3>
+            <div  className="mx-auto">
+                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">CURBSIDE PICK-UP available in as little as one hour!</h3>
             </div>
-            <div  class="mx-auto">
-                <h3 class="text-center">3</h3>
+            <div  className="mx-auto">
+                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">Get your gifts before the holidays!</h3>
             </div>
     </Slider>
-        </div>        
+    
+        </div>   
+        <Menu/>    
       </div>
     );
   }
---
+++ src/components/header/topnavigation/Menu.client.jsx
@@ -0,0 +1,24 @@
+export default function Menu() {
+    return (
+    <div  className="container-2xl test">
+       <header  className=" feature_image secondary_logo--true  home-head">
+          <div  className="header is-absolute  header-fixed--true ">
+             <div  className="top_bar flex">
+                <div  className="main_nav_wrapper1 left">
+                   <div  className="main_nav ">
+                      <div  className="nav1 ">
+                         <ul  className="menu align_left flex">
+                            <li><a href="#"  className="uppercase">TOYS</a></li>
+                            <li><a href="#"  className="uppercase">Books</a></li>
+                            <li><a href="#"  className="uppercase">Baby</a></li>
+                         </ul>
+                      </div>
+                   </div>
+                </div>
+                <div  className="logo logo--image"> <a href="#" title="Mastermind Toys"> <img src="src/assets/logo.png"  className="secondary_logo lazyload" alt="Mastermind Toys" width="135" height="70" /></a></div>
+             </div>
+          </div>
+       </header>
+    </div>
+    );
+    }
