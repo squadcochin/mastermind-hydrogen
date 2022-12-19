38dbd228e52af74a6eb5c8ba6b997914f8d0ede7
cochin@32 <cochin.vsourz@gmail.com>
2022-12-19 19:09:11 +0530

    Footer Section Created

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,8 +1,12 @@
 import Announcement from "../components/header/announcement/announcement.client";
+import Footer from "../components/Footer/Footer.client";
 
 export default function Home() {
   return (
-    <Announcement />
+    <>
+      <Announcement />
+      <Footer />
+    </>
   );
 }
 
