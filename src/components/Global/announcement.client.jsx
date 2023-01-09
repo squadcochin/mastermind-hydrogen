d48558b57c4149653b0d6a625d7a700fbda8eaa1
Sarika <76066066+sarika-123@users.noreply.github.com>
2023-01-09 09:46:51 +0530

    Announcement bar Update

--- src/components/Global/announcement.client.jsx
+++ src/components/Global/announcement.client.jsx
@@ -5,55 +5,32 @@ import './style.css';
 
 
 export function Announcement() {
+  const content = [ "11 FREE SHIPPING* 123 on orders over $35 (shipped within Canada, some exceptions apply)","CURBSIDE PICK-UP available in as little as one hour!",
+  "Get your gifts before the holidays!"];
 
     var settings = {
-
         dots: false,
-
         infinite: true,
-
         speed: 5000,
-
         autoplay:true,
-
         arrows:false,
-
         slidesToShow: 1,
-
         slidesToScroll: 1
-
       };
-
-
 
     return (
       
       <div className="container-2xl">
-
         <div className="announcement-bar-bg">
-
         <Slider {...settings}>
-
+          {content.map(item => (  
             <div className="mx-auto">
-
-                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">11 FREE SHIPPING* 123 on orders over $35 (shipped within Canada, some exceptions apply)</h3>
-
-            </div>
-
-            <div  className="mx-auto">
-
-                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">CURBSIDE PICK-UP available in as little as one hour!</h3>
-
-            </div>
-
-            <div  className="mx-auto">
-
-                <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">Get your gifts before the holidays!</h3>
-
-            </div>
-
-    </Slider>
-
+              <h3 className="text-center h-12 flex items-center justify-center text-white font-semibold">
+                {item}
+              </h3>
+            </div> 
+          ))}  
+      </Slider>
         </div>        
 
       </div>
